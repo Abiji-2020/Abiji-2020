a614409bf68c7079cba21b65b4f1f253188990d4
Abinand P <abinand0911@gmail.com>
2024-11-20 17:21:02 +0530

    Update update_redme.py

--- scrips/update_redme.py
+++ scrips/update_redme.py
@@ -1,1 +1,3 @@
 # Python file to implement the scripting to automatically update the ReadMe. 
+
+''' RoadMap to Implemeting, Create an new file with the updations on the google sheets and to run the workflow per hour'''
