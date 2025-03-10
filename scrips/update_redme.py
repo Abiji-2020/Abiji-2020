a0414f9b254f1a121b6c9505d6b3880e5635d4e5
Abinand P <abinand0911@gmail.com>
2025-03-10 08:57:17 +0530

    Delete scrips/update_redme.py

--- scrips/update_redme.py
+++
@@ -1,3 +0,0 @@
-# Python file to implement the scripting to automatically update the ReadMe. 
-
-''' RoadMap to Implemeting, Create an new file with the updations on the google sheets and to run the workflow per hour'''
